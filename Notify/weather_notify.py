77137402ee6292d5cce5b2b832a45857652b4bc4
AnnChang <59999989+AnnChangAnn@users.noreply.github.com>
2024-10-23 11:26:30 +0800

    fix: newline problem

--- Notify/weather_notify.py
+++ Notify/weather_notify.py
@@ -15,7 +15,7 @@ def lineNotifyWeather(cwa_token):
 
     # set the weather of location
     Location_List = ['臺北市', '新北市', '桃園市', '基隆市', '高雄市']
-    msg = '【今日天氣】\n'
+    msg = '【今日天氣】'
     msg += "\r時間: 6:00~18:00\r"
     msg_weather = ''
 
@@ -33,8 +33,7 @@ def lineNotifyWeather(cwa_token):
         MinT = json_format['records']['location'][0]['weatherElement'][2]['time'][0]['parameter']['parameterName']
         MaxT = json_format['records']['location'][0]['weatherElement'][4]['time'][0]['parameter']['parameterName']
         pop = json_format['records']['location'][0]['weatherElement'][1]['time'][0]['parameter']['parameterName']
-        msg_weather += '\r\n'
-        msg_weather += f"{location}: {weather}\n氣溫: {MinT}度~{MaxT}度\n降雨機率: {pop}%"
+        msg_weather += f"{location}: {weather}\n氣溫: {MinT}度~{MaxT}度\n降雨機率: {pop}%\n"
 
     return msg + msg_weather
     # send request
