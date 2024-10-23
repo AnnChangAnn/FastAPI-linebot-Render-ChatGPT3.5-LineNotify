a9b3324fed48d6efa0c08e5f9f3c8417b5841ec8
AnnChang <59999989+AnnChangAnn@users.noreply.github.com>
2024-10-23 10:38:40 +0800

    Update: weather_notify return msg and use cwa_token with env

--- Notify/weather_notify.py
+++ Notify/weather_notify.py
@@ -1,13 +1,13 @@
 import requests, json
 from datetime import datetime
 
-def lineNotifyWeather(token):
+def lineNotifyWeather(cwa_token):
 
     # test token : VAH3QdBdbkQ7B9blSEpEpjlwA4GR0MKxzbEHhShkse8
     # add headers
-    headers = {
-        "Authorization": "Bearer " + token,
-        "Content-Type": "application/x-www-form-urlencoded"}
+    # headers = {
+    #     "Authorization": "Bearer " + token,
+    #     "Content-Type": "application/x-www-form-urlencoded"}
     
     # get now time
     # NowDate = datetime.now().strftime('%Y-%m-%d')
@@ -15,12 +15,12 @@ def lineNotifyWeather(token):
 
     # set the weather of location
     Location_List = ['臺北市', '新北市', '桃園市', '基隆市', '高雄市']
-    msg = "\r    時間: 6:00~18:00\r"
+    msg = "\r時間: 6:00~18:00\r"
     msg_weather = ''
 
     for i in Location_List[0:]:
         # 氣象局改名為氣象署 cwb => cwa
-        cwbapi = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-E74B046A-CAB4-4570-B4D9-FA3F7F225DC1&locationName={i}&startTime="
+        cwbapi = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={cwa_token}&locationName={i}&startTime="
         cwbr = requests.get(cwbapi)
         print('request success!!')
 
@@ -32,13 +32,10 @@ def lineNotifyWeather(token):
         MinT = json_format['records']['location'][0]['weatherElement'][2]['time'][0]['parameter']['parameterName']
         MaxT = json_format['records']['location'][0]['weatherElement'][4]['time'][0]['parameter']['parameterName']
         pop = json_format['records']['location'][0]['weatherElement'][1]['time'][0]['parameter']['parameterName']
-        msg_weather = msg_weather + f'''
-        
-    {location}: {weather}
-    氣溫: {MinT}度~{MaxT}度
-    降雨機率: {pop}%'''
+        msg_weather = msg_weather + f"\n{location}: {weather}\n氣溫: {MinT}度~{MaxT}度\n降雨機率: {pop}%"
 
+    return msg + msg_weather
     # send request
-    res = {'message': msg + msg_weather}
-    response = requests.post("https://notify-api.line.me/api/notify",
-                      headers=headers, params=res)
+    # res = {'message': msg + msg_weather}
+    # response = requests.post("https://notify-api.line.me/api/notify",
+    #                   headers=headers, params=res)
