fe663abb1c796cd10b309f600d911a14b0b5a753
AnnChang <59999989+AnnChangAnn@users.noreply.github.com>
2025-04-16 10:18:34 +0800

    Update: weather api debug test

--- Notify/weather_notify.py
+++ Notify/weather_notify.py
@@ -1,5 +1,6 @@
-import requests, json
+import requests
 from datetime import datetime
+import time
 
 def lineNotifyWeather(cwa_token):
     # set the weather of location
@@ -12,25 +13,27 @@ def lineNotifyWeather(cwa_token):
         # 氣象局改名為氣象署 cwb => cwa
         print("call cwa api...")
         cwbapi = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={cwa_token}&locationName={i}"
-        print(f"cwbapi : {cwbapi}")
-        cwbr = requests.get(cwbapi)
+        # cwbr = requests.get(cwbapi)
 
-        print(f"get response : {cwbr}")
-        cwbr_dict = cwbr.json()
-        json_string = json.dumps(cwbr_dict)
-        json_format = json.loads(json_string)
-        print(f"get response : {json_format}")
+        for attempt in range(3):  # 最多 retry 三次
+            try:
+                response = requests.get(url, timeout=5)
+                if response.status_code == 200:
+                    break
+                else:
+                    print(f"錯誤狀態碼：{response.status_code}，重試中...")
+            except requests.exceptions.RequestException as e:
+                print(f"連線錯誤：{e}，重試中...")
+                time.sleep(0.1)
+        # cwbr_dict = cwbr.json()
+        # json_string = json.dumps(cwbr_dict)
+        # json_format = json.loads(json_string)
+        json_format = response.json()
         location = json_format['records']['location'][0]['locationName']
-        print(f"location : {location}")
         weather = json_format['records']['location'][0]['weatherElement'][0]['time'][0]['parameter']['parameterName']
-        print(f"weather : {weather}")
         MinT = json_format['records']['location'][0]['weatherElement'][2]['time'][0]['parameter']['parameterName']
-        print(f"MinT : {MinT}")
         MaxT = json_format['records']['location'][0]['weatherElement'][4]['time'][0]['parameter']['parameterName']
-        print(f"MinT : {MaxT}")
         pop = json_format['records']['location'][0]['weatherElement'][1]['time'][0]['parameter']['parameterName']
-        print(f"MinT : {pop}")
         msg_weather += f"\n\n{location}: {weather}\n氣溫: {MinT}度~{MaxT}度\n降雨機率: {pop}%"
-        print(f"msg_weather : {msg_weather}")
 
     return msg + msg_weather + ' \n '
